69710c62a9a1237532cc258211f9dc51966fc4f0
MikulikV <mikulik1995@outlook.com>
2023-07-13 13:56:04 -0400

    Add backend logic

--- cbn.py
+++ cbn.py
@@ -1,4 +1,19 @@
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter
+from langchain.vectorstores import DocArrayInMemorySearch
+from langchain.chains import RetrievalQA,  ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import PyMuPDFLoader
+from langchain.document_loaders import DirectoryLoader
+
 import panel as pn
+import param
+
+from dotenv import load_dotenv 
+
+
+load_dotenv()
 
 # WIDGETS
 
@@ -11,7 +26,7 @@ menu = pn.widgets.RadioButtonGroup(
     button_style="outline",
     styles={"margin-bottom": "20px"}
 )
-select__temperature = pn.widgets.FloatSlider(
+select_temperature = pn.widgets.FloatSlider(
     name="Temperature", 
     start=0.0, 
     end=1.0, 
@@ -33,46 +48,152 @@ select_search_type = pn.widgets.RadioButtonGroup(
     button_style="outline",
     styles={"margin-bottom": "20px"}
 )
-select__top_k = pn.widgets.IntSlider(
+select_top_k = pn.widgets.IntSlider(
     name="Number of relevant chunks",
     start=1, 
     end=10, 
     step=1, 
     value=4, 
-    styles={"font-size": "16px"}
+    styles={"font-size": "16px", "margin-bottom": "30px"}
 )
+setup_button = pn.widgets.Button(name="Save settings", button_type="success", styles={"margin": "0 auto"})
 
 # Main layout
+title = pn.pane.HTML(
+    object=f"<h2>{menu.value}</h2>"
+)
 # Conversation window
 question = pn.widgets.TextInput(
     value="", placeholder="Send a message ...", width=720, height=40, toolbar=False,
 )
 send_button = pn.widgets.Button(name="Send", width=80, height=40)
+clearhistory_button = pn.widgets.Button(name="Clear History", button_type="warning")
 
 
+# Create a CBN Chat chain
+def load_db(source_directory, search_type, chain_type, k, temperature):
+    # load documents
+    loader = DirectoryLoader(source_directory, glob="**/*.pdf", loader_cls=PyMuPDFLoader)
+    documents = loader.load()
+    # split documents
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
+    docs = text_splitter.split_documents(documents)
+    # define embedding
+    embeddings = OpenAIEmbeddings()
+    # create vector database from data
+    db = DocArrayInMemorySearch.from_documents(docs, embeddings)
+    # define retriever
+    retriever = db.as_retriever(search_type=search_type, search_kwargs={"k": k})
+    # create a chatbot chain. Memory is managed externally.
+    qa = ConversationalRetrievalChain.from_llm(
+        llm=ChatOpenAI(model_name="gpt-3.5-turbo", temperature=temperature), 
+        chain_type=chain_type, 
+        retriever=retriever, 
+        return_source_documents=True,
+        return_generated_question=True,
+    )
+    return qa
 
 
-title = pn.pane.HTML(
-    object=f"<h2>{menu.value}</h2>"
-)
+class chatbot(param.Parameterized):
+    chat_history = param.List([])
+    answer = param.String("")
+    db_query  = param.String("")
+    db_response = param.List([])
+    
+    def __init__(self,  **params):
+        super(chatbot, self).__init__( **params)
+        self.panels = []
+        self.source_directory = "docs"
+        self.qa = load_db(self.source_directory ,select_search_type.value, select_chain_type.value, select_top_k.value, select_temperature.value)
+
+    def convchain(self, query):
+        if not query:
+            return pn.widgets.ChatBox(
+                value=self.panels,
+                message_icons={
+                    "You": "assets/user.png",
+                    "Gizmo": "assets/gizmo.png",
+                },
+                show_names=False,
+                allow_input=False,
+            )
+        result = self.qa({"question": query, "chat_history": self.chat_history})
+        self.chat_history.extend([(query, result["answer"])])
+        self.db_query = result["generated_question"]
+        self.db_response = result["source_documents"]
+        self.answer = result['answer'] 
+        self.panels.extend([
+            {"You": query},
+            {"Gizmo": self.answer},
+        ])
+        question.value = ''  #clears loading indicator when cleared
+        return pn.widgets.ChatBox(
+            value=self.panels,
+            message_icons={
+                "You": "assets/user.png",
+                "Gizmo": "assets/gizmo.png",
+            },
+            show_names=False,
+            allow_input=False,
+        )
+        
+
+    @param.depends('db_query')
+    def get_lquest(self):
+        if not self.db_query :
+            return pn.Column(
+                pn.Row(pn.pane.Markdown(f"Last question to DB:", styles={'background-color': '#F6F6F6'})),
+                pn.Row(pn.pane.Str("no DB accesses so far"))
+            )
+        return pn.Column(
+            pn.Row(pn.pane.Markdown(f"DB query:", styles={'background-color': '#F6F6F6'})),
+            pn.pane.Str(self.db_query)
+        )
+
+    @param.depends('db_response')
+    def get_sources(self):
+        if not self.db_response:
+            return 
+        rlist=[pn.Row(pn.pane.Markdown(f"Result of DB lookup:", styles={'background-color': '#F6F6F6'}))]
+        for doc in self.db_response:
+            rlist.append(pn.Row(pn.pane.Str(doc)))
+        return pn.WidgetBox(*rlist, scroll=True)
+
+    @param.depends('convchain', 'clr_history') 
+    def get_chats(self):
+        if not self.chat_history:
+            return pn.WidgetBox(pn.Row(pn.pane.Str("No History Yet")), scroll=True)
+        rlist=[pn.Row(pn.pane.Markdown(f"Current Chat History variable", styles={'background-color': '#F6F6F6'}))]
+        for exchange in self.chat_history:
+            rlist.append(pn.Row(pn.pane.Str(exchange)))
+        return pn.WidgetBox(*rlist, width=600, scroll=True)
+
+    def clr_history(self,count=0):
+        self.chat_history = []
+        return 
+
+cbn = chatbot()
+
+
+clearhistory_button.on_click(cbn.clr_history)
+
+conversation = pn.bind(cbn.convchain, question) 
 
 chat_box = pn.Column(
     pn.Row(question, send_button),
-    pn.widgets.ChatBox(
-        value=[
-            {"You": "Hello!"},
-            {"Gizmo": "Hi, I'm Gizmo, how can I help you today!"},
-        ],
-        message_icons={
-            "You": "assets/user.png",
-            "Gizmo": "assets/gizmo.png",
-        },
-        show_names=False,
-        allow_input=False,
-    )
+    pn.panel(conversation,  loading_indicator=True, height=300)
 )
-database = pn.widgets.ColorPicker(name='Color Picker', value='#99ef78')
-chat_history = pn.widgets.ColorPicker(name='Color Picker', value='#000')
+database = pn.Column(
+    pn.panel(cbn.get_lquest),
+    pn.layout.Divider(),
+    pn.panel(cbn.get_sources ),
+)
+chat_history = pn.Column(
+    pn.panel(cbn.get_chats),
+    pn.layout.Divider(),
+    clearhistory_button,
+)
 
 
 layout = pn.Column(
@@ -94,12 +215,13 @@ template = pn.template.FastGridTemplate(
     sidebar=[
         pn.Column(
             menu,
-            select__temperature,
+            select_temperature,
             pn.pane.HTML("Chain type:", styles={"font-size": "16px", "margin-bottom": "0"}),
             select_chain_type,
             pn.pane.HTML("Search type:", styles={"font-size": "16px", "margin-bottom": "0"}),
             select_search_type,
-            select__top_k,
+            select_top_k,
+            setup_button,
             margin=10
         )
     ],
